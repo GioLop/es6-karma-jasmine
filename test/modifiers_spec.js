0371ce746e9efdf122cee4faf9d34e5c0d948ffc
Giovanni López <glopez@hugeinc.com>
2020-08-31 18:28:47 -0500

    Change get method

--- test/modifiers_spec.js
+++ test/modifiers_spec.js
@@ -2,12 +2,12 @@ import modifiers from '../src/modifiers.js';
 
 describe("Modifiers", () => {
     beforeEach(() => {
-        var dummyElement = document.createElement('div');
-        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
+        const dummyElement = document.createElement('div');
+        document.getElement = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     })
 
     it("should toogle class in element", () => {
-        const element = document.getElementById()
+        const element = document.getElement()
 
         modifiers.toogleClass(element, "open")
         expect(element.classList.contains("open")).toEqual(true)
