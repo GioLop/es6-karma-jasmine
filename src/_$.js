b9abc29fddb5c7df6b6fd2cf1f08aac946bc2886
Mateo Casasbuenas <casasbuenas@google.com>
2020-09-21 17:17:35 -0500

    Merge fix

--- src/_$.js
+++ src/_$.js
@@ -1,17 +1,7 @@
 const _$ = {
-<<<<<<< HEAD
-  head: ([x]) => x,
-  tail: ([, ...y]) => y,
-  copy: arr => [...arr],
-  
-};
-
-export default _$;
-=======
     head: ([x]) => x,
     tail: ([, ...y]) => y,
     copy: arr => [...arr],
     isPresent: (x,y) => x.some(x => y.includes(x))
 };
 export default _$;
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
