65ff4ca1493b79a3f4f6fe4d916ef20a7e4ff840
Mateo Casasbuenas <casasbuenas@google.com>
2020-09-21 17:15:44 -0500

    Fix for merge

--- test/_$_spec.js
+++ test/_$_spec.js
@@ -1,48 +1,25 @@
-<<<<<<< HEAD
-import _$ from '../src/_$.js';
-
-=======
 import _$ from '../src/_$';
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
 describe("Test _$ lib", () => {
   describe("head func", () => {
     it("should return 'a'", () => {
       expect(_$.head(['a', 'b', 'c'])).toEqual('a');
     });
   });
-<<<<<<< HEAD
-  
-=======
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
   describe("tail func", () => {
     it("should return everything but the first element", () => {
       expect(_$.tail(['a', 'b', 'c']).includes('a')).toEqual(false);
     });
   });
-<<<<<<< HEAD
-
-=======
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
   describe("copy func", () => {
     it("should return a new array with the same values", () => {
       const myArr = ['a', 'b', 'c'];
       const newArr = _$.copy(myArr);
       expect(newArr).toEqual(myArr);
     });
-<<<<<<< HEAD
-
-=======
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
     it("should return a new instance of a given array", () => {
       const myArr = ['a', 'b', 'c'];
       const newArr = _$.copy(myArr);
       newArr.push('d');
-<<<<<<< HEAD
-
-      expect(newArr).toEqual(['a', 'b', 'c', 'd']);
-    });
-  });
-=======
       expect(newArr).toEqual(['a', 'b', 'c', 'd']);
     });
   });
@@ -53,5 +30,4 @@ describe("Test _$ lib", () => {
           expect(_$.isPresent(myArr, hasArr)).toEqual(true)
       })
   })
->>>>>>> a919626ef895356ed659bcdc25a92f517ece6506
 });
